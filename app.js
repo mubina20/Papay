776f779682b779fe0fd9a241ef48ba03f1b36c3f
Mubina <mubina3120@gmail.com>
2023-10-12 01:02:04 +0900

    loyihani express qismini shakllantirib olish

---
+++ app.js
@@ -0,0 +1,52 @@
+console.log('Web serverni boshlash');
+
+// express frameworkini install qilib, chaqiriladi
+const exp = require('express');
+
+// express documentationga qarasak, undan instance yasashimiz kerak
+// express() - expressning objecti
+// endi appda foydalanib Express web server quramiz
+const app = exp();
+
+// MongoDBni chaqiramiz
+// .db() -- MongoDBni objecti
+// biz shu orqali DataBasega turli hil ma'lumotlarni yozish, o'chirish, o'qish yoki delete qilish operatsiyalarini amalga oshiramiz
+const db = require('./server').db();
+// mongodbni pasta kerak edi, shuning uchun require qildik
+const mongodb = require('mongodb');
+
+// 1) Kirish code  
+// bu bosqichda - Expressga kirib kelayotgan ma'lumotlarga bog'liq bo'lgan codelar yozidali
+
+// harqanday browserdan kirib kelayotgan requestlar uchun public folderi ochiq, ya'ni faqatgina public folderni ko'ra oladi
+// publicda - frontEndga tegishli bo'lgan JS, CSS, imagelar publicga boradi
+app.use(exp.static('public'));
+
+// kirib kelayotgan JSON formatdagi datalarni objectga o'girib beradi
+app.use(exp.json());
+
+// HTML post qilingan formalarni qabul qiladi
+// bu bo'lmasa qabul qilmaydi
+app.use(exp.urlencoded({ extended: true }));
+// HTML formalari orqali POST so'rovlarini qabul qilish va ulardan kelgan ma'lumotlarni analiz qilish uchun ishlatiladi.
+
+
+
+// 2) Session code
+
+
+
+// 3) Views code    (Express uchun BSSRda folder yasaymiz)
+
+// view yasash uchun folder kerak
+app.set("views", "views"); // views folderni ko'rsatyapmiz
+
+// view engine - ejs ekanini ko'rsatyapmiz
+app.set("view engine", "ejs");
+
+// demak, EJS orqali BackEndda FrontEnd yasaymiz
+
+
+// 4) Routing code
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,53 @@
+const http = require('http'); // core package
+
+// MongoDB Connect (MongoDBni shu yerda ulaymiz)
+// birinchi MongoDBni ulab, keyin serverni ishga tushirsak yaxshi bo'ladi
+
+// mongodb packageni isntall qilib chaqirib olamiz
+const mongodb = require('mongodb'); // external package
+
+
+// mongoDB compassdan --> mongo_atlas 'connection string'ni copy qilib, 
+// 'connectionString'ga tenglab olamiz
+const connectionString = "mongodb+srv://mina:7f1F7WxY1EaEFzjf@cluster0.tepyubl.mongodb.net/Reja";
+
+
+// mongoDBni documentationida 'connect' degan method bor, ichiga 3ta narsani pass qilishimiz kerak:
+    // 1) connection String
+    // 2) {userNewUrlParser: true, useUnifiedTopoligy: true}
+    // 3) callback
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}, (err, client) => { // ikkinchi parameterga data yoki client deb yozvolamiz 
+    // biz MongoDBga ulanganimizda - Error mavjud bo'lmasa MongoDBning clientini, instanceni bizga pass qiladi
+
+    // agar ulanishimiz muvaffaqiyatsiz bo'lsa - console.log 'error' bersin
+    if(err) console.log('ERROR: MongoDBga ulanish muvaffaqiyatsiz boldi!'); 
+    
+    // ask holda, ya'ni ulanishimiz muvaffaqiyatli bo'lsa, error bo'lmasa, bu holda serverimizni boshlaymiz
+    else{ 
+        // DataBasemiz muvaffaqiyatli ulangani uchun serverni boshlayapmiz
+        console.log('MongoDB muvaffaqiyatli ulandi');
+
+        // biz kelajakda 'client' bilan juda ko'p ishlaymiz
+        // shuning uchun 'client'ni export qilib olamiz
+        // 'client' - biz uchun tahlab berilgan DataBase Connection Object
+        module.exports = client; 
+        // console.log(client);
+
+        // app ham, databaseimiz muvaffaqiyatli ulangash require qilinsin
+        const app = require('./app'); // express app, ya'ni app.js boshlandi
+
+        
+        // createServer() --> method - bitta parameter qabul qiladi
+        const server = http.createServer(app);
+
+        // serverni ma'lum bir PORTga listen qildirish
+        let PORT = 3000;
+        server.listen(PORT, function() { // server muvaffaqiyatli amalga oshsa, function ishga tushadi
+            console.log(`server portda muvaffaqiyatli, ${PORT} portda ishlamoqda, http://localhost:${PORT}`);
+        });
+    }
+});
