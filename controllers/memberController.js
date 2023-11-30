2bf8d0ca0f95072e3689e479f8cde05716505463
Mubina <mubina3120@gmail.com>
2023-11-30 15:05:45 +0900

    fix: checkMyAuthentication and retrieve token object data

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -58,13 +58,29 @@ memberController.createToken = (result) => {
 		const upload_data = {
 			_id: result._id,
 			mb_nick: result.mb_nick,
-			mb_type: result.mb_type,
+			mb_type: result.mb_type
 		};
 
 		const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, { expiresIn: '6h' });
 
 		assert.ok(token, Definer.auth_err2);
 		return token;
+	} catch (err) {
+		throw err;
+	}
+};
+
+memberController.checkMyAuthentication = (req, res) => {
+	try {
+		console.log('GET: CheckAuthentication');
+
+		let token = req.cookies['access_token'];
+		// console.log("token:::", token);
+
+		const member = token ? jwt.verify(token, process.env.SECRET_TOKEN) : null;
+		assert.ok(member, Definer.auth_err2);
+
+		res.json({ state: 'Muvaffaqiyatli', data: member });
 	} catch (err) {
 		throw err;
 	}
