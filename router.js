65dd55463369bd1c58bc0bde03e419b0f9a7d3eb
Mubina <mubina3120@gmail.com>
2023-12-02 18:48:41 +0900

    feat: Develop getAllProducts REST API business logic

--- router.js
+++ router.js
@@ -1,12 +1,14 @@
 const express = require('express'); 
 const router = express.Router(); 
 const memberController = require("./controllers/memberController");
+const productController = require("./controllers/productController");
 
 
 /**************************************************************************
  *             REST API(REACT uchun kerak bo'lgan router)                 *
  **************************************************************************/
 
+// Member related routers
 router.post("/signup",  memberController.signup); 
 router.post("/login", memberController.login);
 router.get("/logout",memberController.logout);
@@ -19,13 +21,12 @@ router.get(
     memberController.getChosenMember
 );
 
-// boshqa routerlar
-router.get("/menu", (req, res) => { 
-    res.send("Menu sahifadasiz");
-});
 
-router.get("/community", (req, res) => { 
-    res.send("Jamiyat sahifadasiz");
-});
+// Product related routers
+router.post(
+    "/products", 
+    memberController.retrieveAuthMember,
+    productController.getAllProducts
+);
 
 module.exports = router; 
